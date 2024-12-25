77f5791677156ee0215fba1f0c156aaba912e8fe
extroverter-542 <sanjisama62728@genvolvo.lol>
2024-12-25 12:50:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import pyromod.listen
 from pyrogram import Client, filters
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message
+from pyrogram.errors.exceptions.flood_420 import FloodWait
 import sys
 from datetime import datetime
 import requests
@@ -37,50 +38,57 @@ class Bot(Client):
         self.LOGGER = LOGGER
 
     async def start(self):
-        await super().start()
-        usr_bot_me = await self.get_me()
-        self.uptime = datetime.now()
-
-        # Verify user
-        user_id = usr_bot_me.id
-        if not await verify_user(user_id):
-            self.LOGGER(__name__).info("User verification failed.")
-            return
-
-        if FORCE_SUB_CHANNEL:
+        while True:
             try:
-                link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
-                if not link:
-                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
-                    link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
-                self.invitelink = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel: {FORCE_SUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
-                sys.exit()
-        try:
-            db_channel = await self.get_chat(CHANNEL_ID)
-            self.db_channel = db_channel
-            test = await self.send_message(chat_id=db_channel.id, text="Test Message")
-            await test.delete()
-        except Exception as e:
-            self.LOGGER(__name__).warning(e)
-            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
-            sys.exit()
+                await super().start()
+                usr_bot_me = await self.get_me()
+                self.uptime = datetime.now()
 
-        self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
-        print(ascii_art)
-        print("""Welcome to CodeXBotz File Sharing Bot""")
-        self.username = usr_bot_me.username
-        # web-response
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
+                # Verify user
+                user_id = usr_bot_me.id
+                if not await verify_user(user_id):
+                    self.LOGGER(__name__).info("User verification failed.")
+                    return
+
+                if FORCE_SUB_CHANNEL:
+                    try:
+                        link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
+                        if not link:
+                            await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
+                            link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
+                        self.invitelink = link
+                    except Exception as a:
+                        self.LOGGER(__name__).warning(a)
+                        self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
+                        self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel: {FORCE_SUB_CHANNEL}")
+                        self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+                        sys.exit()
+                try:
+                    db_channel = await self.get_chat(CHANNEL_ID)
+                    self.db_channel = db_channel
+                    test = await self.send_message(chat_id=db_channel.id, text="Test Message")
+                    await test.delete()
+                except Exception as e:
+                    self.LOGGER(__name__).warning(e)
+                    self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
+                    self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+                    sys.exit()
+
+                self.set_parse_mode(ParseMode.HTML)
+                self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
+                print(ascii_art)
+                print("""Welcome to CodeXBotz File Sharing Bot""")
+                self.username = usr_bot_me.username
+                # web-response
+                app = web.AppRunner(await web_server())
+                await app.setup()
+                bind_address = "0.0.0.0"
+                await web.TCPSite(app, bind_address, PORT).start()
+                break  # Exit the loop if successful
+
+            except FloodWait as e:
+                self.LOGGER(__name__).warning(f"FloodWait: Have to wait for {e.x} seconds.")
+                await asyncio.sleep(e.x)  # Wait for the specified duration
 
     async def stop(self, *args):
         await super().stop()
